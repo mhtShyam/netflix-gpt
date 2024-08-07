ae674be1644d3494975d316c25f4de7ca8a20c5f
shyam <shyammht3@gmail.com>
2024-08-07 20:05:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl border border-dashed border-blue bg-gray-400">
+      hello
+    </div>
+  );
+}
+
+export default App;
